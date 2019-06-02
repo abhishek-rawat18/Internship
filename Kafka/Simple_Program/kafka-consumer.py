0646217079608f461bd2b582bda532bb8eabea56
Abhishek Rawat <46537436+abhishek-rawat18@users.noreply.github.com>
2019-06-02 12:22:29 +0530

    Update and rename Kafka/kafka-consumer.py to Kafka/Simple_Program/kafka-consumer.py

--- Kafka/Simple_Program/kafka-consumer.py
+++ Kafka/Simple_Program/kafka-consumer.py
@@ -9,6 +9,11 @@ consumer = KafkaConsumer(
      group_id='my-group',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
+#auto_offset_reset as earliest means that the consumer starts reading at the latest committed offset.
+#enable_auto_commit as True makes sure that the consumer commits its read offset every interval.
+#value_deserializer deserializes the data into a common json format, 
+#the inverse of what our value serializer was doing in producer.
+
 for message in consumer:
     message = message.value
     print('{}'.format(message))
